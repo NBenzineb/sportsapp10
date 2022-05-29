64b1707e31e0a7bb1c24346b68c149b7e6e90b94
Nadia Benzineb <91059761+NBenzineb@users.noreply.github.com>
2022-05-29 20:08:58 +0100

    Mongodb connected

---
+++ src/components/bodyPartSelector.test.js
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import BodyPartSelector from './bodyPartSelector';
+  
+test('invites the user to choose their workout', () => {
+  render(<BodyPartSelector />);
+  const text = screen.getByText("Choose your workout:");
+  expect(text).toBeInTheDocument();
+});
+
+test('user can select a body part', () => {
+  let spy = jest.fn();
+  render(<BodyPartSelector updateSelected={spy}/>);
+  const checkbox = screen.getByLabelText("Upper Body");
+  fireEvent.click(checkbox);
+  console.log(spy.mock.calls);
+  expect(spy.mock.calls.length).toEqual(1);
+  expect(spy.mock.calls[0][0]).toEqual({"upperBody": true});
+  //expect(text).toBeInTheDocument();
+  // send data to App.js
+});
+
+test('user can deselect body part', () => {
+  let spy = jest.fn();
+  render(<BodyPartSelector updateSelected={spy}/>);
+  const checkbox = screen.getByLabelText("Upper Body");
+  fireEvent.click(checkbox);
+  fireEvent.click(checkbox);
+
+  expect(spy.mock.calls.length).toEqual(2);
+  expect(spy.mock.calls[1][0]).toEqual({});
+  
+});
+//user can select a body part
+//user can deselect body parts
+//user can select multiple body parts
