907aea685051add81f3b41f37bd4cdb87cc52b85
Nadia Benzineb <91059761+NBenzineb@users.noreply.github.com>
2022-05-30 20:22:08 +0100

    navbar working- orange background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
 import React, { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar"
 import BodyPartSelector from './components/bodyPartSelector';
+import Myworkouts from "./components/myworkouts";
 
 function App() {
   const [selectedBodyParts, updateSelectedBodyParts] = useState({}); 
 
   return (
     <div className="App">
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Myworkouts} />
+        <Route path="/about" component={Myworkouts} />
+        <Route path="/contact" component={Myworkouts} />
+        <Route path="/signin" component={Myworkouts} />
+        <Route path="/sign-up" component={Myworkouts} />
+      </Routes>
+    </Router>
       <BodyPartSelector updateSelected={updateSelectedBodyParts}/>
       <p>{JSON.stringify(selectedBodyParts)}</p>
+      
+    
     </div>
   );
 }
 
 export default App;
+
+
+
