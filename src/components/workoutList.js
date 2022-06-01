a57df440fb84295a442beffb6018cd12b83e07e5
Joe <j.newman@live.co.uk>
2022-06-01 10:41:20 +0100

    commented out lists for different selections on workoutlist, to check information about selected is still passed there

--- src/components/workoutList.js
+++ src/components/workoutList.js
@@ -3,38 +3,39 @@ import WorkoutCard from './workoutCard.js'
 
 
 function WorkoutList(props) {
-  if (props.selected.upperBody === true) {
-    let workouts = upperBody 
-    return (
-      <div className="cards">
-        {workouts.map((workout) => (
-          <WorkoutCard workoutName={`${workout.workoutName}`} workoutLogo={`${workout.workoutLogo}`} exercises={`${workout.exercises}`}/>
-        ))}
-      </div>
-  );
-  } else if (props.selected.lowerBody === true) {
-    let workouts = lowerBody
-    return (
-      <div className="cards">
-        {workouts.map((workout) => (
-          <WorkoutCard workoutName={`${workout.workoutName}`} workoutLogo={`${workout.workoutLogo}`} exercises={`${workout.exercises}`}/>
-        ))}
-      </div>
-  );
-  } else if (props.selected.fullBody === true) {
-    let workouts = fullBody
-    return (
-      <div className="cards">
-        {workouts.map((workout) => (
-          <WorkoutCard workoutName={`${workout.workoutName}`} workoutLogo={`${workout.workoutLogo}`} exercises={`${workout.exercises}`}/>
-        ))}
-      </div>
-  );
-  } else {
+  console.log(props.selected);
+  // if (props.selected.upperBody === true) {
+  //   let workouts = upperBody 
+  //   return (
+  //     <div className="cards">
+  //       {workouts.map((workout) => (
+  //         <WorkoutCard workoutName={`${workout.workoutName}`} workoutLogo={`${workout.workoutLogo}`} exercises={`${workout.exercises}`}/>
+  //       ))}
+  //     </div>
+  // );
+  // } else if (props.selected.lowerBody === true) {
+  //   let workouts = lowerBody
+  //   return (
+  //     <div className="cards">
+  //       {workouts.map((workout) => (
+  //         <WorkoutCard workoutName={`${workout.workoutName}`} workoutLogo={`${workout.workoutLogo}`} exercises={`${workout.exercises}`}/>
+  //       ))}
+  //     </div>
+  // );
+  // } else if (props.selected.fullBody === true) {
+  //   let workouts = fullBody
+  //   return (
+  //     <div className="cards">
+  //       {workouts.map((workout) => (
+  //         <WorkoutCard workoutName={`${workout.workoutName}`} workoutLogo={`${workout.workoutLogo}`} exercises={`${workout.exercises}`}/>
+  //       ))}
+  //     </div>
+  // );
+  // } else {
     return (
       <h3>No body parts selected yet</h3>
     )
-  }
+  // }
 
     
   
