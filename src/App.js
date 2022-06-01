2c9197bff0e581db52ba79fecf11a40b5fd9b316
Ruzeka <98535979+R552-beep@users.noreply.github.com>
2022-06-01 16:04:07 +0100

    Revert "Fixed formatting"

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import './App.css';
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/navbar"
-
+import BodyPartSelector from './components/bodyPartSelector';
 import Myworkouts from "./components/myworkouts";
-import Workouts from "./components/workouts";
+import WorkoutList from './components/workoutList';
 import Login from './components/login';
 import SignUp from './components/signup';
 
 
 
 function App() {
+  const [selectedBodyParts, updateSelectedBodyParts] = useState({}); 
   
+
   return (
     <div className="App">
       <Router>
       <Navbar />
         <Routes>
           <Route path="/" exact component={Myworkouts} />
-          <Route path="/workouts" element={<Workouts/>} />
+          <Route path="/allworkouts" component={BodyPartSelector} />
           <Route path="/myworkouts" element={<Myworkouts/>} />
           <Route path="/signin" element={<Login/>} />
           <Route path="/sign-up" element={<SignUp/>} />
         </Routes>
       </Router>
-     
+      <BodyPartSelector updateSelected={updateSelectedBodyParts}/>
+      <p>{JSON.stringify(selectedBodyParts)}</p>
+      <WorkoutList selected={selectedBodyParts}/>
     </div>
   );
 }
