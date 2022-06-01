2c85a71c410f437bdc318aac4ae0a82d6834d3dd
Joe <j.newman@live.co.uk>
2022-06-01 10:56:51 +0100

    styling image not working

--- src/components/workoutCard.js
+++ src/components/workoutCard.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function WorkoutCard({workoutName, workoutLogo, exercises}) {
   const exerciseArray = exercises.split(",")
+  // const imageStyles = { maxWidth: 10, maxHeight: 10 };
+
 
   return (
     <div className="card">
       <h3>{workoutName}</h3>
-      <img src= {workoutLogo}/>
+      <img styles={{ width: 10, height: 10, borderRadius: 10 }} src= {workoutLogo} alt={''}/>
       <ul className="exerciseList">
         {exerciseArray.map((exercise) => (
           <li>{exercise}</li>
