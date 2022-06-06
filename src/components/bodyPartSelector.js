40aabd74dedeeb835699b488ca13162a8145b270
Joe <j.newman@live.co.uk>
2022-06-06 17:08:16 +0100

    reactive checkboxes, but updateSelecetd not changing

--- src/components/bodyPartSelector.js
+++ src/components/bodyPartSelector.js
@@ -7,25 +7,24 @@ function BodyPartSelector({updateSelected}) {
 
 
   const clickHandler = (bodyPart) => {
-    const theState = isChecked;
-    console.log(theState)
-    const previousValue = theState[bodyPart]
-    console.log(previousValue)
-    theState[bodyPart] = !previousValue
-    console.log(theState)
-    // setIsChecked(theState)
-    updateSelected(theState);
+    const previousValue = isChecked[bodyPart];
+    console.log(previousValue);
+    console.log(isChecked);
+    isChecked[bodyPart] = !previousValue;
+    console.log(isChecked);
+    setIsChecked(isChecked);
+    updateSelected(isChecked);
   }
 
   return (
     <div className="checkboxes">
       <p>Choose your workout:</p>
       <ul>
-        <input type="checkbox" id="upper_body" name="upper_body" value="upper_body" onChange={clickHandler("upperBody")}/>
+        <input type="checkbox" id="upper_body" name="upper_body" value="upper_body"  onChange={() => clickHandler("upperBody")}/>
         <label htmlFor="upper_body"> Upper Body</label>
-        <input type="checkbox" id="lower_body" name="lower_body" value="lower_body" onChange={clickHandler("lowerBody")}/>
+        <input type="checkbox" id="lower_body" name="lower_body" value="lower_body"  onChange={() => clickHandler("lowerBody")}/>
         <label htmlFor="lower_body"> Lower Body</label>
-        <input type="checkbox" id="full_body" name="full_body" value="full_body" onChange={clickHandler("fullBody")}/>
+        <input type="checkbox" id="full_body" name="full_body" value="full_body" onChange={() => clickHandler("fullBody")}/>
         <label htmlFor="full_body"> Full Body</label>
         
       </ul>
