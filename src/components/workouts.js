dadb9e197c18ce6a1ad4ee0cce45b5a90b160146
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-06-07 13:22:17 +0100

    edit function working

--- src/components/workouts.js
+++ src/components/workouts.js
@@ -6,12 +6,11 @@ import BodyPartSelector from './bodyPartSelector';
 import WorkoutList from './workoutList';
 
 function Workouts() {
-  const [selectedBodyParts, updateSelectedBodyParts] = useState({}); 
+  const [selectedBodyParts, updateSelectedBodyParts] = useState({"upperBody": false, "lowerBody": false, "fullBody": false}); 
     return (
     <div>
       <BodyPartSelector updateSelected={updateSelectedBodyParts}/>
-      <p>{JSON.stringify(selectedBodyParts)}</p>
-      <WorkoutList selected={selectedBodyParts}/>
+      <WorkoutList selected={selectedBodyParts.bodyParts}/>
     </div>
 
 )
